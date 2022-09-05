d8046f1badd04c968e16a5e71a386268c175f27f
L2HYUNN <dhl9810@naver.com>
2022-09-05 14:20:21 +0900

    #12 Data Types

--- The Complete JS Course 2022/01-Fundamentals-Part-1/starter/script.js
+++ The Complete JS Course 2022/01-Fundamentals-Part-1/starter/script.js
@@ -9,8 +9,24 @@ assign their values according to your own country (population in millions)
 2. Log their values to the console
 */
 let country, continent, population;
-country = "안산";
-continent = "12";
+country = "경기도";
+continent = "안산";
 population = 2022;
 
 console.log(country, continent, population);
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
