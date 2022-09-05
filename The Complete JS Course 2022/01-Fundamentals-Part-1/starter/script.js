9d2d665cf5885e903c840a0892eb50717fb6b46e
L2HYUNN <dhl9810@naver.com>
2022-09-05 13:28:45 +0900

    #10 Values and Variables

--- The Complete JS Course 2022/01-Fundamentals-Part-1/starter/script.js
+++ The Complete JS Course 2022/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,16 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript Beginner");
 console.log(40 + 5);
+
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+let country, continent, population;
+country = "안산";
+continent = "12";
+population = 2022;
+
+console.log(country, continent, population);
